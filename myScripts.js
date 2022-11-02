e7fc534ef9c455b6cc649cc48a81a3ccd09db496
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-11-02 07:22:43 +0100

    simple push to merge

---
+++ myScripts.js
@@ -0,0 +1,42 @@
+// timer
+
+function countdown()
+{
+	var today = new Date();
+		
+	var day = today.getDate();
+		if (day<10) day = "0"+day;
+	var month = today.getMonth()+1;
+		if (month<10) month = "0"+month;
+	var year = today.getFullYear();
+		
+	var hour = today.getHours();
+		if (hour<10) hour = "0"+hour;
+		
+	var minute = today.getMinutes();
+		if (minute<10) minute = "0"+minute;
+		
+	var second = today.getSeconds();
+		if (second<10) second = "0"+second;
+		
+	document.getElementById("clock").innerHTML = 
+	day+"/"+month+"/"+year+" "+hour+":"+minute+":"+second;
+		 
+		setTimeout("countdown()",1000);	
+}
+
+// przycisk "Git Commit" chowa element na stronie
+$(document).ready(function(){
+    $("#gitButton").click(function(){
+        $(".form").toggle(1500);
+    });
+});
+
+// ustawia wysokość diva nav i ad względem wysokości diva content
+$(document).ready(function(){
+    let divHeight = $("#content").height(); 
+        $("#nav").css('min-height', divHeight+'px');
+    let divHeight2 = $("#content").height(); 
+        $("#ad").css('min-height', divHeight2+'px');
+});
+
