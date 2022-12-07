825461c35141636544265db789fecfbf8860d191
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-07 13:34:50 +0100

    carousel - struggling

---
+++ new version/js/birdRecognizer.js
@@ -0,0 +1,23 @@
+const birdMonth = ["notKnow", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const birdLocation = ["notKnow", "cityArchitecture", "cityGreenery", "lakeShore", "riverBank", "seaBeach", "mountainRock"];
+const birdSize = ["notKnow", "mouse", "cat", "dog"];
+const birdColor1 = ["notKnow", "brown", "dark", "white"];
+const birdColor2 = ["notKnow", "brown", "dark", "white"];
+const birdNumber = ["notKnow", "single", "inPair", "smallFlock", "mediumFlock", "bigFlock"];
+const birdActivity = ["notKnow", "walksAround", "fliesAround", "swimsAround"]; 
+const birdSound = ["notKnow", "co", "hoot", "whistle"];
+const birdFeature = ["notKnow", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+const perkozDwuczuby = {
+    birdMonth:"birdMonth[3, 4, 5, 6, 7, 8, 9, 10, 11]", 
+    birdLocation:"birdLocation[3]", 
+    birdSize: "birdSize[2] ",
+    birdColor1: "birdColor1 [1]",
+    birdColor2: "birdColor2 [3]",
+    birdNumber: "birdNumber[1, 2]",
+    birdActivity: "birdActivity [3]", 
+    birdSound: "birdSound[4]",
+    birdFeature: "birdFeature[0]",
+}
+console.log(birdColor1[1]);
