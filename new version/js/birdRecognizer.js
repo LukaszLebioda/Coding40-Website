8a967981854eba45ba6ebb694a21646f92cc4e38
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-14 16:00:28 +0100

    pigeons added

--- new version/js/birdRecognizer.js
+++ new version/js/birdRecognizer.js
@@ -1,7 +1,8 @@
 
 // coś zrobić, żeby wyświetlało komunikat: "Sorry, I couldn't find a match"
-// i musi być przycisk "TRY AGAIN" w takim wypadku!
 
+// coś zrobić, żeby wyświetlało komunikat: "Sorry, I couldn't find a match"
+// i żeby reloadowało nie do main page, a do właściwego accordiona z programem;
 
 //let ifBird;
 
@@ -16,6 +17,7 @@
                         // })
 
 const kos = {
+    sequence: 1,
     name: "Blackbird (kos)",
     description: "One of the best singers out there, often to be heard composing from the top of a tree all day long. Tends to focus on singing so much, that may lose vigilance and become an easy pray for predators.",
     rights: "Photo by Zeynel Cebeci, CC BY-SA 4.0",
@@ -23,6 +25,7 @@ const kos = {
 }
 
 const szpak = {
+    sequence: 2,
     name: "Starling (szpak)",
     description: "Produces futuristic sounds an is able to imitate other birds, even simple human words. A group of starlings can deplete a cherry-tree fruit deposit within minutes.",
     rights: "Photo by Frank Schulenburg, CC BY-SA 4.0",
@@ -30,6 +33,7 @@ const szpak = {
 }
 
 const trznadel = {
+    sequence: 3,
     name: "Yellowhammer (trznadel)",
     description: "Sings persistently in the middle of hot summer day, when no other species sing. Supposedly inspired Beethoven to compose the 5th Symphony.",
     rights: "Photo by Andreas Trepte, www.avi-fauna.info",
@@ -37,6 +41,7 @@ const trznadel = {
 }
 
 const wrobel = {
+    sequence: 4,
     name: "Sparrow (wróbel)",
     description: "Once one of the largest, the sparrow population is shrinking rapidly due to vanishing of old farm countryside. Fortunately sparrow is a very fertile creature...",
     rights: "Photo by CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=1719233",
@@ -44,6 +49,7 @@ const wrobel = {
 }
 
 const mazurek = {
+    sequence: 5,
     name: "Tree-sparrow (mazurek)",
     description: "Similar to sparrow, but a bit smaller, with brown hat and black spots on cheeks. Less attached to humans, yet still tends to visit bird feeders in winter.",
     rights: "Photo by Andreas Trepte, CC BY-SA 2.5",
@@ -51,6 +57,7 @@ const mazurek = {
 }
 
 const sojka = {
+    sequence: 6,
     name: "Jay (sójka)",
     description: "Its noisy squawks alarm other inhabitants of the forest, so it's not hunters' favourite bird. Tends to bury ahorns in the ground thus spreading oak seeds all over the place.",
     rights: "Photo by Luc Viatour, CC BY-SA 3.0",
@@ -58,10 +65,59 @@ const sojka = {
 }
 
 const kowalik = {
+    sequence: 7,
     name: "Nuthatch (kowalik)",
     description: "The only European bird that can climb down head first. Blocks a tree hollow entrance with a mixture of saliva and clay to protect chicks from predators sneaking in.",
     rights: "Photo by Isiwal/Wikimedia Commons/CC BY-SA 4.0",
     photo: "./img/birds/kowalik.jpg"
+}
+
+const bocianBialy = {
+    sequence: 8,
+    name: "White stork (bocian biały)",
+    description: "Contrary to legends white storks don't bring babies. They are ruthless predators that hunt earthworms, insects, rodents, chicks and yes... frogs as well.",
+    rights: "Photo by Frank Vassen, CC BY 2.0",
+    photo: "./img/birds/bocianbialy.jpg"
+}
+
+const rybitwaRzeczna = {
+    sequence: 9,
+    name: "Common tern (rybitwa rzeczna)",
+    description: "Often seen when cicrling above rivers or lakes and diving into water from the air with huge speed only to re-emerge with a fish in the beak. Lays eggs on the sandy ground on small islands or hidden shores.",
+    rights: "Photo by Tony Hisgett, CC BY 2.0",
+    photo: "./img/birds/rybitwarzeczna.jpg"
+}
+
+const dudek = {
+    sequence: 10,
+    name: "Hoopoe (dudek)",
+    description: "A fold-out fan and exotic colours distinct this bird form other species. In many European languages its name reflects the sounds it produces.",
+    rights: "Photo by J.M.Garg - CC BY-SA 3.0",
+    photo: "./img/birds/dudek.jpg"
+}
+
+const golabMiejski = {
+    sequence: 11,
+    name: "City pigeon (gołąb miejski)",
+    description: "This pigeon is able to drink water without tilting its head up! It's a descendant of one rock pigeon - the world's oldest domesticated bird mentioned by both Mesopotamian cuneiforms as well as Egyptian hieroglyphics.",
+    rights: "Photo by Muhammad Mahdi Karim - Own work, GFDL 1.2,",
+    photo: "./img/birds/golabmiejski.jpg"
+}
+
+const grzywacz = {
+    sequence: 12,
+    name: "Wood pigeon (grzywacz)",
+    description: "Bigger than city pigeon but less city-related, with white stains on both sides of the neck and flies away before winter. Produces 5-syllable melody what distincts it from other pigeon species.",
+    rights: "Photo by Tristan Ferne, CC BY 2.0",
+    photo: "./img/birds/grzywacz.jpg"
+}
+
+const sierpowka = {
+    sequence: 13,
+    name: "Collared dove (sierpówka)",
+    description: "First seen in Poland back in 1940, came from Asia. Similar to city pigeon but with a black collar around the neck and produces 3-syllable monotonous melody.",
+    rights: "Photo by Adrian Pingstone, public domain",
+    photo: "./img/birds/sierpowka.jpg"
 }
 
 // ---------------------------------------------------------------------
@@ -71,7 +127,7 @@ const placeBird = (name, description, photo, rights) => {
                             const birdDiv = document.querySelector("#birdResultDiv");
 
                             const birdHeadingName = document.createElement("h4");
-                            const birdHeadingDescription = document.createElement("h5");
+                            const birdHeadingDescription = document.createElement("h6");
                             const birdHeadingImage = document.createElement("img");
                             const birdHeadingRights = document.createElement("h6");
                             const birdEmptyDiv = document.createElement("span");
@@ -97,10 +153,17 @@ const placeBird = (name, description, photo, rights) => {
 }
 
 let radios = document.querySelectorAll('.divBirdRadioButton input[type="radio"]');
-let button = document.querySelector('#buttonBirds');
+let button = document.querySelector("#buttonBirds");
+let button2 = document.querySelector("#buttonBirds2");
 const bp = []; // stands for bird permutation
 
-button.addEventListener('click', () => {
+button2.addEventListener("click", () => {
+    document.getElementById("buttonBirds").disabled = false;
+    document.getElementById("buttonBirds2").disabled = true;
+    location.reload();
+})
+
+button.addEventListener("click", () => {
     if(document.querySelectorAll('.divBirdRadioButton input[type="radio"]:checked').length < 6) {
         alert("One or more categories remain unchecked. Please revisit the questions");
     } else {
@@ -109,9 +172,10 @@ button.addEventListener('click', () => {
 		    if (radio.checked) {
                 bp.push(radio.value);
                 document.getElementById("buttonBirds").disabled = true; 
+                document.getElementById("buttonBirds2").disabled = false; 
                 console.log(bp);
                 
-                    // 1 trznadel
+                    // 3 trznadel
                     if(
                         // season 1-4
                         (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
@@ -147,7 +211,7 @@ button.addEventListener('click', () => {
                             placeBird(szpak.name, szpak.description, szpak.photo, szpak.rights);
                         }
 
-                    // 3 kos
+                    // 1 kos
                     if(
                         // season 1-4
                         (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
@@ -183,7 +247,7 @@ button.addEventListener('click', () => {
                             placeBird(wrobel.name, wrobel.description, wrobel.photo, wrobel.rights);
                         }  
                 
-                    // 4 mazurek
+                    // 5 mazurek
                     if(
                         // season 1-4
                         (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
@@ -201,7 +265,7 @@ button.addEventListener('click', () => {
                             placeBird(mazurek.name, mazurek.description, mazurek.photo, mazurek.rights);
                         }  
 
-                    // 5 sójka
+                    // 6 sójka
                     if(
                         // season 1-4
                         (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
@@ -219,7 +283,7 @@ button.addEventListener('click', () => {
                             placeBird(sojka.name, sojka.description, sojka.photo, sojka.rights);
                         }  
 
-                    // 6 kowalik
+                    // 7 kowalik
                     if(
                         // season 1-4
                         (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
@@ -235,6 +299,118 @@ button.addEventListener('click', () => {
                         && (bp.includes("37") || bp.includes("39") || bp.includes("41"))) 
                         {
                             placeBird(kowalik.name, kowalik.description, kowalik.photo, kowalik.rights);
+                        }  
+
+                    // 8 bocian biały
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2") || bp.includes("3")) 
+                        // location 5-12
+                        && (bp.includes("7") || bp.includes("9")) 
+                        // size 13-18
+                        && bp.includes("18") 
+                        // color 19-27
+                        && (bp.includes("21")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("29") || bp.includes("30") || bp.includes("31") || bp.includes("32") || bp.includes("33")) 
+                        // activity 34-43
+                        && (bp.includes("38") || bp.includes("40") || bp.includes("42") || bp.includes("43"))) 
+                        {
+                            placeBird(bocianBialy.name, bocianBialy.description, bocianBialy.photo, bocianBialy.rights);
+                        }  
+
+                    // 9 rybitwa rzeczna
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2") || bp.includes("3")) 
+                        // location 5-12
+                        && (bp.includes("5") || bp.includes("6") || bp.includes("7")) 
+                        // size 13-18
+                        && bp.includes("15") 
+                        // color 19-27
+                        && (bp.includes("20") || bp.includes("23")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("30") || bp.includes("31") || bp.includes("32")) 
+                        // activity 34-43
+                        && (bp.includes("36") || bp.includes("38") || bp.includes("40"))) 
+                        {
+                            placeBird(rybitwaRzeczna.name, rybitwaRzeczna.description, rybitwaRzeczna.photo, rybitwaRzeczna.rights);
+                        }  
+
+                    // 10 dudek
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2")) 
+                        // location 5-12
+                        && (bp.includes("7") || bp.includes("8") || bp.includes("9")) 
+                        // size 13-18
+                        && (bp.includes("14") || bp.includes("15")) 
+                        // color 19-27
+                        && (bp.includes("21") || bp.includes("26") || bp.includes("27")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("30")) 
+                        // activity 34-43
+                        && (bp.includes("39") || bp.includes("40") || bp.includes("42") || bp.includes("43"))
+                        ) 
+                        {
+                            placeBird(dudek.name, dudek.description, dudek.photo, dudek.rights);
+                        }  
+
+                    // 11 gołąb miejski
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2") || bp.includes("3") || bp.includes("4")) 
+                        // location 5-12
+                        && (bp.includes("7") || bp.includes("11") || bp.includes("12")) 
+                        // size 13-18
+                        && (bp.includes("15")) 
+                        // color 19-27
+                        && (bp.includes("23") || bp.includes("24")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("30") || bp.includes("31") || bp.includes("32") || bp.includes("33")) 
+                        // activity 34-43
+                        && (bp.includes("38") || bp.includes("39") || bp.includes("40") || bp.includes("42") || bp.includes("43"))
+                        ) 
+                        {
+                            placeBird(golabMiejski.name, golabMiejski.description, golabMiejski.photo, golabMiejski.rights);
+                        }  
+
+                    // 12 grzywacz
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2") || bp.includes("3")) 
+                        // location 5-12
+                        && (bp.includes("7") || bp.includes("8") || bp.includes("9") || bp.includes("11") || bp.includes("12")) 
+                        // size 13-18
+                        && (bp.includes("15") || bp.includes("16")) 
+                        // color 19-27
+                        && (bp.includes("23") || bp.includes("24")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("30") || bp.includes("31")) 
+                        // activity 34-43
+                        && (bp.includes("39") || bp.includes("40") || bp.includes("42") || bp.includes("43"))
+                        ) 
+                        {
+                            placeBird(grzywacz.name, grzywacz.description, grzywacz.photo, grzywacz.rights);
+                        }  
+
+                    // 13 sierpówka
+                    if(
+                        // season 1-4
+                        (bp.includes("1") || bp.includes("2") || bp.includes("3")) 
+                        // location 5-12
+                        && (bp.includes("8") || bp.includes("9") || bp.includes("11") || bp.includes("12")) 
+                        // size 13-18
+                        && bp.includes("15")
+                        // color 19-27
+                        && (bp.includes("23") || bp.includes("24")) 
+                        // numbers 28-33
+                        && (bp.includes("28") || bp.includes("30") || bp.includes("31")) 
+                        // activity 34-43
+                        && (bp.includes("39") || bp.includes("40") || bp.includes("42") || bp.includes("43"))
+                        ) 
+                        {
+                            placeBird(sierpowka.name, sierpowka.description, sierpowka.photo, sierpowka.rights);
                         }  
 
 
