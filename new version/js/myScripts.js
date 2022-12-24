958e142dfbb67f807f27ebe877f07d0d60629c74
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-24 23:14:45 +0100

    copy to clipboard succesfully added to cypress test source code

--- new version/js/myScripts.js
+++ new version/js/myScripts.js
@@ -1,11 +1,18 @@
-const button = document.querySelector("clipboardButton")
-button.addEventListener("click", () => {
-    
-  const copyText = document.getElementById("sourceCodeClipboard");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
+// copy texts (e.g. source code pieces) to CLIPBOARD:
+const copyButton = document.querySelector("#clipboardButton");
+copyButton.addEventListener("click", function () {
 
-  navigator.clipboard.writeText(copyText.value);
+  const copyButton2 = document.createElement("button");
+  copyButton2.innerHTML = "<i class='bi bi-clipboard-check'></i>";        
 
-  alert("Copied the text: " + copyText.value);
-}
+  copyButton.parentNode.replaceChild(copyButton2, copyButton);
+
+          navigator.clipboard
+              .writeText(document.getElementById("sourceCodeClipboard").innerText)
+              .then(
+                  success => console.log("text copied"), err => console.log("error copying text")
+              )
+})
+
+
+                  
