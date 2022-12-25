c1c9f89fd811b439d81135f5cf3058abf51d6b0c
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-25 15:58:02 +0100

    jQuery replaceWith

--- new version/js/myjQuery.js
+++ new version/js/myjQuery.js
@@ -2,11 +2,21 @@
 //    alert("Ready!"); 
 // })
 
+// first photo: hide / showe / replaceWith
 $(document).ready(function(){
- $("#buttonjqHide").click(function(){
-   $("img#jqHide").toggle(1500);
+ $("#buttonjqToggle").click(function(){
+   $("#buttonjqToggle").hide(1500);
+   $("img#jqToggle").hide(1500);
+   $("#buttonjqToggle2").show( "<i class='bi bi-arrow-counterclockwise'></i>" );
  });
 });
+$(document).ready(function(){
+    $("#buttonjqToggle2").click(function(){
+      $("#buttonjqToggle2").hide(1500);
+      $("img#jqToggle").show(1500);
+      $("#buttonjqToggle").show( "<i class='bi bi-question'></i>" );
+    });
+   });
 
 
 
