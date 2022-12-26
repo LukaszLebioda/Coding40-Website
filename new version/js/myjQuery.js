a793e4c818efdc175be0bf3d176809fdebb7630d
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-26 15:23:21 +0100

    TestLink test cases, jQuery practice

--- new version/js/myjQuery.js
+++ new version/js/myjQuery.js
@@ -4,19 +4,68 @@
 
 // first photo: hide / showe / replaceWith
 $(document).ready(function(){
- $("#buttonjqToggle").click(function(){
-   $("#buttonjqToggle").hide(1500);
-   $("img#jqToggle").hide(1500);
-   $("#buttonjqToggle2").show( "<i class='bi bi-arrow-counterclockwise'></i>" );
+ $("#jqButton1").click(function(){
+   $("#jqButton1").hide(1500);
+   $("img#jqImage1").hide(1500);
+   $("span#jqSpan1").hide(1500);
+   $("#jqButton2").show( "<i class='bi bi-arrow-counterclockwise'></i>" );
+   $("span#jqSpan2").show(1500);
  });
 });
 $(document).ready(function(){
-    $("#buttonjqToggle2").click(function(){
-      $("#buttonjqToggle2").hide(1500);
-      $("img#jqToggle").show(1500);
-      $("#buttonjqToggle").show( "<i class='bi bi-question'></i>" );
+    $("#jqButton2").click(function(){
+      $("#jqButton2").hide(1500);
+      $("span#jqSpan2").hide(1500);
+      $("span#jqSpan1").show(1500);
+      $("img#jqImage1").show(1500);
+      $("#jqButton1").show(1500);
     });
    });
 
+// second photo: css property (image darkness, text change)
+$(document).ready(function(){
+
+  let dark = 1;
+
+     $("#jqButton3").click(function(){
+      dark -= 0.1;
+      $("img#jqImage2").css({"-webkit-filter" : "brightness("+dark+")"})
+      $("span#jqSpan4").show();
+      $("#jqButton4").show();
+      $("span#jqSpan3").hide();
+    });
+
+    $("#jqButton4").click(function(){
+      dark = 1;
+      $("span#jqSpan3").show();
+      $("span#jqSpan4").hide();
+      $("img#jqImage2").css("-webkit-filter", "brightness(1)")
+      $("#jqButton4").hide();
+    });
+
+});
+
+// third photo: replace images left right
+$(document).ready(function(){
+  $("#jqButton5").click(function(){
+    $("img#jqImage3").hide();
+    $("img#jqImage4").show();
+    $("span#jqSpan6").show();
+    $("span#jqSpan5").hide();
+    $("#jqButton5").hide();
+    $("#jqButton6").show();
+  });
+$("#jqButton6").click(function(){
+    $("img#jqImage4").hide();
+    $("img#jqImage3").show();
+    $("span#jqSpan5").show();
+    $("span#jqSpan6").hide();
+    $("#jqButton6").hide();
+    $("#jqButton5").show();
+  });
+ });
 
 
+
+
+// fourth photo = figcaption change (3 versions)
